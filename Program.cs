d994371eaa6d343846078bbee40c4b7ef3151deb
VitoriaRossi <vitoria.rossi555@gmail.com>
2023-10-29 17:49:35 -0300

    Aprendi sobre interpolação e concatenação de Strings.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information;
+string cidade = "Mongaguá";
+string estado = "SP";
+
+string mensagem = $"A Etec AB fica localizada em {cidade}/{estado}.";
+
+string novaMensagem = mensagem
+ .Replace("e","x")
+ .Replace("E", "X");
+
+Console.WriteLine(mensagem.ToUpper());
+Console.WriteLine(mensagem.ToLower());
+
+Console.WriteLine(novaMensagem);
